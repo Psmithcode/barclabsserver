cd9ff247471f5adf1a58f8fbdc1eec64ac8a8e1a
Psmithcode <peytonsmith53@gmail.com>
2023-04-03 15:23:13 -0500

    updated refresh token

--- sql/connection.js
+++ sql/connection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "bar-c-kennels.c7hvzmah9kx9.us-east-1.rds.amazonaws.com",
-  user: "admin",
-  password: "ACA2023!",
-  database: "db1",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   port: 3306,
 });
 pool.query("select now()", function (err, results) {

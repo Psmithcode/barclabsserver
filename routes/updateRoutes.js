4a52a811332f97ecce9e4117cd529d8f5c3a962f
Psmithcode <peytonsmith53@gmail.com>
2023-04-10 12:55:53 -0500

    fixed routing issue with updates

--- routes/updateRoutes.js
+++ routes/updateRoutes.js
@@ -3,3 +3,4 @@ const express = require("express");
 let router = new express.Router();
 let controller = require("../controllers/updateController");
 router.post("/updates", controller.createUpdate);
+module.exports = router;
